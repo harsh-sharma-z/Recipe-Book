c0fd23862977247fbeae65392b351992aecf6ee9
harsh-sharma-z <harsh.ramakant.sharma@gmail.com>
2023-11-13 01:14:57 +0530

    added recipe and shopping list

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,7 +7,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./recipe-list.component.css'],
 })
 export class RecipeListComponent {
-  recipes = [];
+  recipes: Recipe[] = [
+    new Recipe(
+      'A test recipe',
+      'This is simply a test.',
+      'https://cdn-icons-png.flaticon.com/512/3565/3565418.png'
+    ),
+  ];
 
   constructor() {}
 
--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.model';
 
 @Component({
   selector: 'app-shopping-list',
   templateUrl: './shopping-list.component.html',
-  styleUrls: ['./shopping-list.component.css']
+  styleUrls: ['./shopping-list.component.css'],
 })
 export class ShoppingListComponent {
+  ingredients: Ingredient[] = [
+    new Ingredient('Paneer', 5),
+    new Ingredient('Tomatos', 6),
+    new Ingredient('Onions', 3),
+  ];
 
+  constructor() {}
+
+  ngOnInit() {}
 }

ccf645e5f9b558821d3fdec023aec7a8b11550a1
harsh-sharma-z <harsh.ramakant.sharma@gmail.com>
2023-11-23 13:14:29 +0530

    Adde toggle details

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -17,8 +17,8 @@ export class RecipeListComponent {
     ),
     new Recipe(
       'Shahee Paneer',
-      'This is a royal recipe made with curry, paneer and cashwe nuts.',
-      'https://cdn-icons-png.flaticon.com/512/3565/3565418.png'
+      'This is a royal recipe made with curry, paneer and cashew nuts.',
+      'https://t3.ftcdn.net/jpg/02/28/28/22/360_F_228282257_CmwCG8Gxt0mOypQJ9UdflnXba5JJth2v.jpg'
     ),
   ];
 
